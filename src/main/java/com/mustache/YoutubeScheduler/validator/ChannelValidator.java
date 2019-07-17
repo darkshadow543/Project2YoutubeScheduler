87f42ba2c3e531980a9d5e89504498b72f21181a
darkshadow543 <sbengel@hotmail.com>
2019-07-16 20:20:12 -0400

    Finishe Channel Line, Finished Validators

--- src/main/java/com/mustache/YoutubeScheduler/validator/ChannelValidator.java
+++ src/main/java/com/mustache/YoutubeScheduler/validator/ChannelValidator.java
@@ -2,13 +2,18 @@ package com.mustache.YoutubeScheduler.validator;
 
 import org.springframework.stereotype.Component;
 
+import com.mustache.YoutubeScheduler.exception.ValidatorException;
 import com.mustache.YoutubeScheduler.model.Channel;
 
 @Component
 public class ChannelValidator {
 
-	public void ValidateChannel(Channel chan) {
-		
+	public void ValidateChannel(Channel chan) throws ValidatorException {
+		if (chan.getName() != null || chan.getName().equals("")){
+			throw new ValidatorException("Name cannot be null");
+		} else if (chan.getOwner() == null) {
+			throw new ValidatorException("Name cannot be null");
+		}
 	}
 
 }
--- src/main/java/com/mustache/YoutubeScheduler/validator/SubscriptionValidator.java
+++ src/main/java/com/mustache/YoutubeScheduler/validator/SubscriptionValidator.java
@@ -2,11 +2,22 @@ package com.mustache.YoutubeScheduler.validator;
 
 import org.springframework.stereotype.Component;
 
+import com.mustache.YoutubeScheduler.exception.ValidatorException;
+import com.mustache.YoutubeScheduler.model.Subscription;
+
 @Component
 public class SubscriptionValidator {
 
 	public SubscriptionValidator() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void ValidateSub(Subscription sub) throws ValidatorException {
+		if (sub.getChannel() == null) {
+			throw new ValidatorException("Name cannot be null");
+		} else if (sub.getUser() == null) {
+			throw new ValidatorException("Name cannot be null");
+		}
 	}
 
 }
