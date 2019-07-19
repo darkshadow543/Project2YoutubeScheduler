3232510861c4aaa73660583c529c13f68732d13c
Joshua Fitzsimmons <josh.fitzsimmons@eagles.oc.edu>
2019-07-18 21:03:24 -0400

    added the video controller

--- src/main/java/com/mustache/YoutubeScheduler/controller/VideoController.java
+++ src/main/java/com/mustache/YoutubeScheduler/controller/VideoController.java
@@ -1,12 +1,59 @@
 package com.mustache.YoutubeScheduler.controller;
 
-import org.springframework.stereotype.Controller;
 
-@Controller
-public class VideoController {
+import java.util.List;
 
-	public VideoController() {
-		// TODO Auto-generated constructor stub
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.mustache.YoutubeScheduler.model.Video;
+import com.mustache.YoutubeScheduler.service.VideoService;
+
+@CrossOrigin
+
+@RestController
+@RequestMapping("videos")public class VideoController {
+
+	@Autowired
+	private VideoService videoserv;
+	
+	@PostMapping()
+	public ResponseEntity<?> register(@RequestBody Video vid) {
+		System.out.println("VideoController->save");
+		videoserv.createVideo(vid);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public @ResponseBody void update(@PathVariable("id") Integer id,@RequestBody Video vid) {
+		System.out.println("UserController->update" + id);
+		videoserv.update(vid);
+	}
+	
+	@GetMapping()
+	public List<Video> list() {
+		List<Video> list = videoserv.getAll();
+		return list;
 	}
 
+	@GetMapping("/{id}")
+	public Video findOne(@PathVariable("id") Integer id) {
+		Video video = videoserv.getVideo(id);
+		return video;
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable("id") Integer id) {
+		videoserv.delete(id);
+	}
 }
